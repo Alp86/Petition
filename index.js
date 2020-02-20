68527b0492e64c8617fff81cd158bb197b3e341a
Alp86 <alperen.kan.86@gmail.com>
2020-02-20 22:32:05 +0100

    slight changes

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-////////////////////// Set Up /////////////////////
+////////////////////// Imports /////////////////////
 const express = require('express');
 const hb = require('express-handlebars');
 const cookieSession = require('cookie-session');
 const csurf = require('csurf');
 const { countSignatures } = require('./utils/db');
-// const { hash, compare } = require('./utils/bc');
-// const { requireLoggedOutUser, requireSignature, requireNoSignature} = require("./utils/middleware");
 
+///////////////////////////////////////////////////
+
+////////////////////// Global variables /////////////////////
 let numSigs = 0;
-
 countSignatures().then(result => {
     if (result.rows[0]) {
         numSigs = result.rows[0].numSigs;
     }
 });
 
-exports.incNumSigs = () => { numSigs++; };
-exports.decNumSigs = () => { numSigs--; };
-exports.getNumSigs = () => { return numSigs; };
+///////////////////////////////////////////////////
 
+/////////////////// Set up ///////////////////
 const app = express();
-
 app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
+
 ///////////////////////////////////////////////////
 
 /////////////////// Middle Ware ///////////////////
@@ -56,312 +55,30 @@ app.use((req, res, next) => {
 });
 ////////////////////////////////////////////////////
 
-module.exports.app = app;
+////////////////////// Exports /////////////////////
+exports.incNumSigs = () => { numSigs++; };
+exports.decNumSigs = () => { numSigs--; };
+exports.getNumSigs = () => { return numSigs; };
+exports.app = app;
+
+////////////////////////////////////////////////////
+
 ///////////////////// Routes //////////////////////
 app.get("/", (req, res) => {
     return res.redirect("/register");
 });
-
-///////////// register /////////////
-// app.get("/register", requireLoggedOutUser, (req, res) => {
-//     res.render("register", {
-//         layout: "main",
-//         login: false
-//     });
-// });
-//
-// app.post("/register", requireLoggedOutUser, (req, res) => {
-//     const { first, last, email, password } = req.body;
-//
-//     hash(password).then(hashedPw => {
-//         insertUser(first, last, email, hashedPw)
-//             .then(result => {
-//                 const user_id = result.rows[0].id;
-//                 req.session.user = {
-//                     userID: user_id,
-//                     first: first,
-//                     last: last,
-//                 };
-//                 return res.redirect("/profile");
-//             })
-//             .catch(error => {
-//                 console.log("error in insertUser: ", error);
-//                 res.render("register", {
-//                     layout: "main",
-//                     login: false,
-//                     message: "Ups something went wrong. Please try again!"
-//                 });
-//             });
-//     });
-// });
-
-
-/////////////////////////////////
-
-///////////// login /////////////
-// app.get("/login", requireLoggedOutUser, (req, res) => {
-//     res.render("login", {
-//         layout: "main",
-//         login: false
-//     });
-// });
-//
-// app.post("/login", requireLoggedOutUser, (req, res) => {
-//     let { email, password } = req.body;
-//     selectUser(email).then(result => {
-//         if (!result.rows[0]) {
-//             res.render("login", {
-//                 layout: "main",
-//                 login: false,
-//                 message: "No account found with that email address."
-//             });
-//         } else {
-//             return compare(password, result.rows[0].password)
-//                 .then(pass => {
-//                     if (pass) {
-//                         req.session.user = {
-//                             userID: result.rows[0].id,
-//                             first: result.rows[0].first,
-//                             last: result.rows[0].last,
-//                             signatureID: result.rows[0].signatureID
-//                         };
-//                         if (result.rows[0].signatureID) {
-//                             res.redirect("/petition");
-//                         } else {
-//                             res.redirect("/thanks");
-//                         }
-//                     }
-//                     else {
-//                         res.render("login", {
-//                             layout: "main",
-//                             message: "Wrong password."
-//                         });
-//                     }
-//                 }).catch(err => {
-//                     console.log("error in compare:", err);
-//                 });
-//         }
-//     }).catch(err => {
-//         console.log("error in selectUser:", err);
-//     });
-// });
 require("./routes/auth");
-/////////////////////////////////
-
-///////////// profile /////////////
-// app.get("/profile", (req, res) => {
-//     res.render("profile", {
-//         layout: "main",
-//         login: true
-//     });
-// });
-//
-// app.post("/profile", (req, res) => {
-//     const { age, city, url } = req.body;
-//
-//     if (!age && !city && !url) {
-//         res.redirect("/petition");
-//     } else {
-//         if (url) {
-//             if (
-//                 !url.startsWith("http://") &&
-//                 !url.startsWith("https://") &&
-//                 !url.startsWith("//")
-//             ) {
-//                 return res.redirect("petition");
-//             }
-//         }
-//         insertUserProfile(age, city, url, req.session.user.userID)
-//             .then( () => {
-//                 res.redirect("petition");
-//             })
-//             .catch(err => {
-//                 console.log("error in insertUserProfile:", err);
-//             });
-//
-//     }
-//
-// });
-/////////////////////////////////
-
-/////////// petition ///////////
-// app.get("/petition", requireNoSignature, (req, res) => {
-//     res.render("petition", {
-//         layout: "main",
-//         login: true
-//     });
-// });
-//
-// app.post("/petition", requireNoSignature, (req, res) => {
-//     let {signature} = req.body;
-//     // insert submitted data into database
-//     insertSignature(req.session.user.userID, signature)
-//         // if there is no error
-//         .then(result => {
-//             console.log("insertSignature result:", result);
-//             let signatureID = result.rows[0].id;
-//             // sets cookie to remember
-//             req.session.user.signatureID = signatureID;
-//             numSigs++;
-//             // redirects to thank you page
-//             res.redirect("/thanks");
-//         })
-//         // if there is an error petition.handlebars is rendered with an error message
-//         .catch(err => {
-//             console.log("error in insertSignature:", err);
-//             res.render("petition", {
-//                 layout: "main",
-//                 login: true,
-//                 message: "Ups something went wrong. Please try again!"
-//             });
-//         });
-// });
-/////////////////////////////////
-
-///////////// thanks /////////////
-// app.get("/thanks", requireSignature, (req, res) => {
-//     selectSignature(req.session.user.userID)
-//         .then(result => {
-//             const signature = result.rows[0].signature;
-//             res.render("thanks", {
-//                 layout: "main",
-//                 login: true,
-//                 numSigs: numSigs,
-//                 signature: signature
-//             });
-//         }).catch(err => {
-//             console.log("error in selectSignature:", err);
-//         });
-// });
-//////////////////////////////////
 require("./routes/petition");
-///////////// signers /////////////
-// app.get("/signers", requireSignature, (req, res) => {
-//     selectSigners().then( result => {
-//         console.log("selectSigners result:", result);
-//         let signers = result.rows;
-//         res.render("signers", {
-//             layout: "main",
-//             login: true,
-//             signers
-//         });
-//     }).catch(err => {
-//         console.log("error in selectSigners:", err);
-//     });
-// });
-//
-// app.get("/signers/:city", requireSignature, (req, res) => {
-//     selectSignersByCity(req.params.city)
-//         .then( result => {
-//             console.log("selectSignersByCity result:", result);
-//
-//             let signers = result.rows;
-//             res.render("signers", {
-//                 layout: "main",
-//                 login: true,
-//                 signers
-//             });
-//         }).catch(err => {
-//             console.log("error in selectSignersByCity:", err);
-//         });
-// });
-//////////////////////////////////////
-
-// app.get("/profile/edit", (req, res) => {
-//
-//     selectUserProfile(req.session.user.userID)
-//         .then(result => {
-//
-//             const profile = result.rows[0];
-//
-//             res.render("edit", {
-//                 layout: "main",
-//                 login: true,
-//                 profile
-//             });
-//         })
-//         .catch(err => {
-//             console.log("error in selectUserProfile:", err);
-//         });
-// });
-//
-// app.post("/profile/edit", (req, res) => {
-//     let { first, last, email, password, age, city, url} = req.body;
-//
-//     if (url) {
-//         if (
-//             !url.startsWith("http://") &&
-//             !url.startsWith("https://") &&
-//             !url.startsWith("//")
-//         ) {
-//             url = "";
-//         }
-//     }
-//     if (password) {
-//         hash(password).then(hashedPw => {
-//             Promise.all([
-//                 updateUserPW(first, last, email, hashedPw, req.session.user.userID),
-//                 updateProfile(age, city, url, req.session.user.userID)
-//             ])
-//                 .then( () => {
-//                     return res.redirect("/thanks");
-//                 })
-//                 .catch(err => {
-//                     console.log("error in editPW Promise.all:", err);
-//                     res.render("edit", {
-//                         template: "main",
-//                         login: true,
-//                         message: "Ups something went wrong. Please try again!"
-//                     });
-//                 });
-//         }).catch(err => {
-//             console.log("error in hashing password:", err);
-//         });
-//     } else {
-//         Promise.all([
-//             updateUser(first, last, email, req.session.user.userID),
-//             updateProfile(age, city, url, req.session.user.userID)
-//         ])
-//             .then( () => {
-//                 return res.redirect("/thanks");
-//             })
-//             .catch(err => {
-//                 console.log("error in edit Promise.all:", err);
-//                 res.render("edit", {
-//                     template: "main",
-//                     login: true,
-//                     message: "Ups something went wrong. Please try again!"
-//                 });
-//             });
-//     }
-// });
-
 require("./routes/profile");
-
-// app.post("/thanks/delete", (req, res) => {
-//     console.log("received delete request");
-//     deleteSignature(req.session.user.userID)
-//         .then(() => {
-//             console.log("signature has been deleted");
-//             delete req.session.user.signatureID;
-//             console.log("req.session:", req.session.user);
-//             numSigs--;
-//             return res.redirect("/petition");
-//         })
-//         .catch(err => {
-//             console.log("error in deleteSignature:", err);
-//         });
-// });
 require("./routes/thanks");
 
+////////////////////////////////////////////////////
 
-// app.get("/logout", (req, res) => {
-//     delete req.session.user;
-//     res.redirect("/login");
-// });
-
+///////////////// Start the engine /////////////////
 if (require.main === module) {
     app.listen(process.env.PORT || 8080, () => console.log(
         "petition server up and running"
     ));
 }
+
+///////////////////////////////////////////////////
---
+++ public/autocomplete.js
@@ -0,0 +1,42 @@
+var autocomplete;
+
+var componentForm = {
+    locality: 'long_name',
+    country: 'long_name',
+};
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search predictions to
+    // geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'), {types: ['geocode']}
+    );
+
+    // Avoid paying for data that you don't need by restricting the set of
+    // place fields that are returned to just the address components.
+    autocomplete.setFields(['address_component']);
+
+    // When the user selects an address from the drop-down, populate the
+    // address fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+
+    for (var component in componentForm) {
+        document.getElementById(component).value = '';
+        document.getElementById(component).disabled = false;
+    }
+
+    // Get each component of the address from the place details,
+    // and then fill-in the corresponding field on the form.
+    for (var i = 0; i < place.address_components.length; i++) {
+        var addressType = place.address_components[i].types[0];
+        if (componentForm[addressType]) {
+            var val = place.address_components[i][componentForm[addressType]];
+            document.getElementById(addressType).value = val;
+        }
+    }
+}
